de362f8fec350519826a9fc98449d902f0948604
Nicole Reed <nicolereed@Nicoles-MacBook-Air.local>
2021-09-22 14:44:01 +0300

    round 2 bih

--- practice.js
+++ practice.js
@@ -1,7 +1,10 @@
-//remove all values from list a, which are present in list b keeping their order.
+//remove all values from list a, which are present in list b keeping their order. 
+//use a set
 function arrayDiff(a, b) {
-    return a.filter(element => !b.includes(element))
+    const set = new Set(b)
+    return a.filter(element => !set.has(element))
 }
+console.log(arrayDiff([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [2, 4, 6, 8, 10]))
 
 //number of times you must multiply the digits in num until you reach a single digit.
 function persistence(num) {
@@ -15,6 +18,7 @@ function persistence(num) {
 
     return count
 }
+console.log(persistence(45))
 
 //return true if the walk will take you exactly ten minutes and return to your starting point. each block takes 1 min to walk.
 function isValidWalk(walk) {
@@ -45,6 +49,7 @@ function order(words) {
     const sorted = arr.sort((a, b) => a.match(/\d/) - b.match(/\d/))
     return sorted.join(' ')
 }
+console.log(order('butt3 cat4 d2og'))
 
 //find out which one of the given numbers in the list (string) differs from the others
 function iqTest(numbers) {
@@ -55,6 +60,7 @@ function iqTest(numbers) {
     return odd.length > even.length ? arr.indexOf(even[0]) + 1 : arr.indexOf(odd[0]) + 1
 }
 
+//return index where each sum of items on either side of index is the same
 function findEvenIndex(arr) {
     const reducer = (acc, current) => acc + current
     let left = [0]
@@ -75,3 +81,85 @@ function findEvenIndex(arr) {
     return -1
 }
 console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1]))
+
+
+//converts dash/underscore delimited words into camel casing,first word capitalized only if the original word was capitalized
+function toCamelCase(str) {
+    const arr = str.split(/[\W\_]/g)
+    const words = [arr[0]]
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i].charCodeAt(0) > 90) {
+            const firstLetter = arr[i].slice(0, 1).toUpperCase()
+            words.push(firstLetter + arr[i].slice(1))
+        } else {
+            words.push(arr[i])
+        }
+
+    }
+    return words.join('')
+}
+
+console.log(toCamelCase("the_stealth-warrior"))
+
+//pangram contains all letters of alphabet. return true if its a pangram
+function isPangram(string) {
+    const arr = string.toLowerCase().split(/[\W\_\d]/g).join('').split('')
+    const set = new Set(arr)
+    return set.size === 26
+}
+console.log(isPangram("The quick brown fox jumps over the lazy dog."))
+
+//return true if the sum of its own digits equals sum of each raised to the power of the number of digits (ex 153) 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+function narcissistic(value) {
+    let sum = 0
+    const digits = value.toString().split('')
+    const power = digits.length
+    digits.map(digit => sum += Math.pow(parseInt(digit), power))
+    return sum === value
+}
+console.log(narcissistic(153))
+
+//iterate over objs, return the name values in a list with the last 2 names seperated by '&'
+function list(names) {
+    const people = names.map(item => item.name)
+    if (people.length == 1) {
+        return people[0]
+    }
+    if (people.length == 2) {
+        return `${people[0]} & ${people[1]}`
+    }
+    if (people.length > 2) {
+        const beginning = people.slice(0, people.length - 2)
+        const ending = people.slice(people.length - 2)
+        return `${beginning.join(', ')}, ${ending.join(' & ').trim()}`
+    }
+    return ''
+}
+console.log(list([{ name: 'Derek' }, { name: 'Nicky' }, { name: 'Blanco' }, { name: 'BB' }]))
+
+
+//find the unique number, take time complexity into account
+function findUniq(arr) {
+    const sorted = arr.sort((a, b) => b - a)
+    console.log(sorted)
+    if (sorted[0] !== sorted[1] && sorted[0] !== sorted[2]) {
+        return sorted[0]
+    }
+    return sorted[sorted.length - 1]
+}
+console.log(findUniq([3, 3, 7, 3, 3, 3, 3]))
+
+
+//split string into pairs. if odd number of characters replace the missing second character of the final pair with an underscore ('_').
+function solution(str) {
+    if (!str) { return [] }
+    let string
+    str.length % 2 === 0 ? string = str : string = str + '_'
+    return string.match(/[a-z\D]{2}/g)
+}
+console.log(solution('aajnvkenruhgk'))
+
+
+
+
+
